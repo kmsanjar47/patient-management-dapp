36ddd94e62a55ff6a8a2489433e903beaddcdf17
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-04-15 09:27:43 +0600

    Events

--- client/web3/web3Actions.ts
+++ client/web3/web3Actions.ts
@@ -1,38 +1,39 @@
-import Web3, { AbiItem } from "web3";
-import PatientManagement from "../../build/contracts/PatientManagement.json";
+import Web3, { AbiItem } from 'web3';
+import PatientManagement from '../../build/contracts/PatientManagement.json';
+
 let web3: Web3;
-if (typeof window !== "undefined" && window.ethereum) {
-  web3 = new Web3(window.ethereum);
+if (typeof window !== 'undefined' && window.ethereum) {
+	web3 = new Web3(window.ethereum);
 } else {
-  console.error("Web3 cannot be initialized: window.ethereum is not available");
+	console.error('Web3 cannot be initialized: window.ethereum is not available');
 }
 
 // Contract address
-const contractAddress = "0x2790139d3343F164786BA3a360809C68a252C711"; // Update with your contract address
+const contractAddress = '0x35C9dE87554a58a786a8C7f007e1D208129FB8ea'; // Update with your contract address
 
 // Create contract instance
 let contract: any;
 
 export function connectMetamask(): Promise<string> | undefined {
-  // web3 = new Web3(window.ethereum);
+	// web3 = new Web3(window.ethereum);
 
-  if (window.ethereum) {
-    return new Promise((resolve, reject) => {
-      web3.eth.requestAccounts().then((accounts) => {
-        console.log("Metamask connected successfully");
-        console.log(accounts);
-        resolve(accounts[0]);
-        contract = new web3.eth.Contract(
-          PatientManagement.abi as AbiItem[],
-          contractAddress
-        );
-      });
-    });
-  } else {
-    Promise.reject("Install Metamask");
-  }
+	if (window.ethereum) {
+		return new Promise((resolve, reject) => {
+			web3.eth.requestAccounts().then((accounts) => {
+				console.log('Metamask connected successfully');
+				console.log(accounts);
+				resolve(accounts[0]);
+				contract = new web3.eth.Contract(PatientManagement.abi as AbiItem[], contractAddress);
+			});
+		});
+	} else {
+		Promise.reject('Install Metamask');
+	}
 }
 
+export function getContract() {
+	return contract;
+}
 // function deployContract(){
 //     const contract = new web3.eth.Contract(abi);
 //     const deploy = contract.deploy({ data: bytecode, arguments: [1] });
@@ -41,229 +42,203 @@ export function connectMetamask(): Promise<string> | undefined {
 
 // Function to store patient data
 export const storePatientData = async (
-  age: number,
-  gender: number,
-  district: string,
-  symptomsDetails: string,
-  vaccineStatus: number,
-  isDead: boolean
+	age: number,
+	gender: number,
+	district: string,
+	symptomsDetails: string,
+	vaccineStatus: number,
+	isDead: boolean
 ) => {
-  // Use Metamask or other provider for user address
-  const accounts = await web3.eth.getAccounts();
-  const userAddress = accounts[0]; // Get first account from Metamask or provider
+	// Use Metamask or other provider for user address
+	const accounts = await web3.eth.getAccounts();
+	const userAddress = accounts[0]; // Get first account from Metamask or provider
 
-  // Send transaction to store patient data
-  await contract.methods
-    .storePatientData(
-      age,
-      gender,
-      district,
-      symptomsDetails,
-      vaccineStatus,
-      isDead
-    )
-    .send({ from: userAddress });
+	// Send transaction to store patient data
+	await contract.methods.storePatientData(age, gender, district, symptomsDetails, vaccineStatus, isDead).send({ from: userAddress });
 };
 
-export const storeAdminData = async (
-  refAddress: string,
-  age: number,
-  gender: number,
-  district: string
-) => {
-  // Use Metamask or other provider for user address
-  const accounts = await web3.eth.getAccounts();
-  const userAddress = accounts[0]; // Get first account from Metamask or provider
+export const storeAdminData = async (refAddress: string, age: number, gender: number, district: string) => {
+	// Use Metamask or other provider for user address
+	const accounts = await web3.eth.getAccounts();
+	const userAddress = accounts[0]; // Get first account from Metamask or provider
 
-  // Send transaction to store admin data
-  await contract.methods
-    .storeAdminData(refAddress, age, gender, district)
-    .send({ from: userAddress });
+	// Send transaction to store admin data
+	await contract.methods.storeAdminData(refAddress, age, gender, district).send({ from: userAddress });
 };
 
 // Function to update vaccine status by admin
-export const updateVaccineStatus = async (
-  patientId: number,
-  vaccineStatus: number
-) => {
-  // Use Metamask or other provider for admin address
-  const accounts = await web3.eth.getAccounts();
-  const adminAddress = accounts[0]; // Get first account from Metamask or provider
+export const updateVaccineStatus = async (patientId: number, vaccineStatus: number) => {
+	// Use Metamask or other provider for admin address
+	const accounts = await web3.eth.getAccounts();
+	const adminAddress = accounts[0]; // Get first account from Metamask or provider
 
-  // Send transaction to update vaccine status
-  await contract.methods
-    .updateVaccineStatus(patientId, vaccineStatus)
-    .send({ from: adminAddress });
+	// Send transaction to update vaccine status
+	await contract.methods.updateVaccineStatus(patientId, vaccineStatus).send({ from: adminAddress });
 };
 
 // Function to update dead status by admin
-export const updateDeadStatus = async (
-  patientId: number,
-  deadStatus: boolean
-) => {
-  // Use Metamask or other provider for admin address
-  const accounts = await web3.eth.getAccounts();
-  const adminAddress = accounts[0]; // Get first account from Metamask or provider
+export const updateDeadStatus = async (patientId: number, deadStatus: boolean) => {
+	// Use Metamask or other provider for admin address
+	const accounts = await web3.eth.getAccounts();
+	const adminAddress = accounts[0]; // Get first account from Metamask or provider
 
-  // Send transaction to update dead status
-  await contract.methods
-    .updateDeadStatus(patientId, deadStatus)
-    .send({ from: adminAddress });
+	// Send transaction to update dead status
+	await contract.methods.updateDeadStatus(patientId, deadStatus).send({ from: adminAddress });
 };
 
 // Function to get patient data list
 export const getPatientDataList = async () => {
-  // Call contract method to get patient data list
-  const data = await contract.methods.getPatientDataList().call();
-  const maxCovidDistrict = findMostFrequentDistrict(data);
-  const iterableList = convertDataToIterableList(data);
-  const medianAges = calculateMedianAgeByDistrict(data);
-  const agePercentage = calculateAgePercentage(data);
-  const averageDeathRate = calculateAverageDeathRate(data);
+	// Call contract method to get patient data list
+	const data = await contract.methods.getPatientDataList().call();
+	const maxCovidDistrict = findMostFrequentDistrict(data);
+	const iterableList = convertDataToIterableList(data);
+	const medianAges = calculateMedianAgeByDistrict(data);
+	const agePercentage = calculateAgePercentage(data);
+	const averageDeathRate = calculateAverageDeathRate(data);
 
-  // console.log(data);
+	// console.log(data);
 
-  return {
-    iterableList,
-    maxCovidDistrict,
-    medianAges,
-    agePercentage,
-    averageDeathRate,
-  };
+	return {
+		iterableList,
+		maxCovidDistrict,
+		medianAges,
+		agePercentage,
+		averageDeathRate,
+	};
 };
 
 export const checkIfAdmin = async (address: string): Promise<boolean> => {
-  const isAdmin = await contract.methods
-    .checkIfAdmin(address)
-    .call({ from: address });
-  return isAdmin;
+	const isAdmin = await contract.methods.checkIfAdmin(address).call({ from: address });
+	return isAdmin;
+};
+
+export const checkIfAlreadyPatient = async (address: string): Promise<boolean> => {
+	const isPatient = await contract.methods.checkIfAlreadyPatient(address).call({ from: address });
+	return isPatient;
 };
 
 /////////////// Internal Actions ///////////////
 
 function convertDataToIterableList(data: any) {
-  const dataList: any = [];
-  data.forEach((item: any) => {
-    dataList.push({
-      "patientId": item.patientId,
-      'age': item.age,
-      'gender': item.gender,
-      'vaccineStatus': item.vaccineStatus,
-      'district': item.district,
-      'symptomsDetails': item.symptomsDetails,
-      'isDead': item.isDead,
-    });
-});
-return dataList;
+	const dataList: any = [];
+	data.forEach((item: any) => {
+		dataList.push({
+			patientId: item.patientId,
+			age: item.age,
+			gender: item.gender,
+			vaccineStatus: item.vaccineStatus,
+			district: item.district,
+			symptomsDetails: item.symptomsDetails,
+			isDead: item.isDead,
+		});
+	});
+	return dataList;
 }
 
 function findMostFrequentDistrict(data: any[]) {
-  // Create an object to store counts of each district
-  const districtCounts: any = {};
+	// Create an object to store counts of each district
+	const districtCounts: any = {};
 
-  // Iterate through the array and count occurrences of each district
-  data.forEach((item) => {
-    const district = item.district;
-    districtCounts[district] = (districtCounts[district] || 0) + 1;
-  });
+	// Iterate through the array and count occurrences of each district
+	data.forEach((item) => {
+		const district = item.district;
+		districtCounts[district] = (districtCounts[district] || 0) + 1;
+	});
 
-  // Find the district with the highest count
-  let mostFrequentDistrict;
-  let maxCount = 0;
-  for (const district in districtCounts) {
-    if (districtCounts[district] > maxCount) {
-      mostFrequentDistrict = district;
-      maxCount = districtCounts[district];
-    }
-  }
+	// Find the district with the highest count
+	let mostFrequentDistrict;
+	let maxCount = 0;
+	for (const district in districtCounts) {
+		if (districtCounts[district] > maxCount) {
+			mostFrequentDistrict = district;
+			maxCount = districtCounts[district];
+		}
+	}
 
-  return mostFrequentDistrict;
+	return mostFrequentDistrict;
 }
 
 // Function to calculate median
 function calculateMedian(values: any) {
-  const sorted = values.slice().sort((a: any, b: any) => {
-    if (typeof a === "bigint" && typeof b === "bigint") {
-      return a < b ? -1 : a > b ? 1 : 0;
-    }
-    return a - b;
-  });
-  const mid = Math.floor(sorted.length / 2);
-  return sorted.length % 2 !== 0
-    ? sorted[mid]
-    : (sorted[mid - 1] + sorted[mid]) / BigInt(2);
+	const sorted = values.slice().sort((a: any, b: any) => {
+		if (typeof a === 'bigint' && typeof b === 'bigint') {
+			return a < b ? -1 : a > b ? 1 : 0;
+		}
+		return a - b;
+	});
+	const mid = Math.floor(sorted.length / 2);
+	return sorted.length % 2 !== 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / BigInt(2);
 }
 
 // Function to calculate median age by district
 function calculateMedianAgeByDistrict(data: any) {
-  const medianAgesByDistrict = [];
+	const medianAgesByDistrict = [];
 
-  // Group data by district
-  const groupedByDistrict: any = {};
-  data.forEach((item: any) => {
-    if (!groupedByDistrict[item.district]) {
-      groupedByDistrict[item.district] = [];
-    }
-    groupedByDistrict[item.district].push(item.age);
-  });
+	// Group data by district
+	const groupedByDistrict: any = {};
+	data.forEach((item: any) => {
+		if (!groupedByDistrict[item.district]) {
+			groupedByDistrict[item.district] = [];
+		}
+		groupedByDistrict[item.district].push(item.age);
+	});
 
-  // Calculate median age for each district
-  for (const district in groupedByDistrict) {
-    const ages = groupedByDistrict[district];
-    const medianAge = calculateMedian(ages);
-    medianAgesByDistrict.push({ district, medianAge });
-  }
+	// Calculate median age for each district
+	for (const district in groupedByDistrict) {
+		const ages = groupedByDistrict[district];
+		const medianAge = calculateMedian(ages);
+		medianAgesByDistrict.push({ district, medianAge });
+	}
 
-  return medianAgesByDistrict;
+	return medianAgesByDistrict;
 }
 
 // Function to calculate the percentage of patients in each age group
 function calculateAgePercentage(patientData: any) {
-  // Initialize counters for each age group
-  let childrenCount = 0;
-  let teenagersCount = 0;
-  let youngCount = 0;
-  let elderCount = 0;
+	// Initialize counters for each age group
+	let childrenCount = 0;
+	let teenagersCount = 0;
+	let youngCount = 0;
+	let elderCount = 0;
 
-  // Iterate through patient data and count patients in each age group
-  patientData.forEach((patient: any) => {
-    const age = patient.age;
-    if (age < 13) {
-      childrenCount++;
-    } else if (age >= 13 && age < 20) {
-      teenagersCount++;
-    } else if (age >= 20 && age < 50) {
-      youngCount++;
-    } else {
-      elderCount++;
-    }
-  });
+	// Iterate through patient data and count patients in each age group
+	patientData.forEach((patient: any) => {
+		const age = patient.age;
+		if (age < 13) {
+			childrenCount++;
+		} else if (age >= 13 && age < 20) {
+			teenagersCount++;
+		} else if (age >= 20 && age < 50) {
+			youngCount++;
+		} else {
+			elderCount++;
+		}
+	});
 
-  // Calculate total number of patients
-  const totalPatients = patientData.length;
+	// Calculate total number of patients
+	const totalPatients = patientData.length;
 
-  // Calculate percentage of patients in each age group
-  const childrenPercentage = (childrenCount / totalPatients) * 100;
-  const teenagersPercentage = (teenagersCount / totalPatients) * 100;
-  const youngPercentage = (youngCount / totalPatients) * 100;
-  const elderPercentage = (elderCount / totalPatients) * 100;
+	// Calculate percentage of patients in each age group
+	const childrenPercentage = (childrenCount / totalPatients) * 100;
+	const teenagersPercentage = (teenagersCount / totalPatients) * 100;
+	const youngPercentage = (youngCount / totalPatients) * 100;
+	const elderPercentage = (elderCount / totalPatients) * 100;
 
-  // Return percentages as an object
-  return {
-    children: childrenPercentage,
-    teenagers: teenagersPercentage,
-    young: youngPercentage,
-    elder: elderPercentage,
-  };
+	// Return percentages as an object
+	return {
+		children: childrenPercentage,
+		teenagers: teenagersPercentage,
+		young: youngPercentage,
+		elder: elderPercentage,
+	};
 }
 
 function calculateAverageDeathRate(data: any) {
-  let totalDeathRate = 0;
-  data.forEach((item: any) => {
-    if (item.isDead) {
-      totalDeathRate++;
-    }
-  });
-  console.log("Total Death Rate:", totalDeathRate);
-  return totalDeathRate / data.length;
+	let totalDeathRate = 0;
+	data.forEach((item: any) => {
+		if (item.isDead) {
+			totalDeathRate++;
+		}
+	});
+	return totalDeathRate / data.length;
 }
--- contracts/patientManagement.sol
+++ contracts/patientManagement.sol
@@ -10,7 +10,9 @@ contract PatientManagement {
 
     User[] public patientList;
     mapping(address => uint) isAdmin;
-    mapping(address => bool) isAlreadyPatient;
+    mapping(address => uint) isAlreadyPatient;
+    event updatedDeadStatus (uint patientId, bool isDead);
+    event updatedVaccineStatus (uint patientId, VaccineStatus vaccineStatus);
 
     struct User {
         uint256 patientId;
@@ -48,7 +50,7 @@ contract PatientManagement {
 
     modifier validPatientAddress(address _patientAddress) {
         require(
-            isAlreadyPatient[_patientAddress] == true,
+            isAlreadyPatient[_patientAddress] == 0,
             "Patient Already Exists!"
         );
         _;
@@ -69,8 +71,78 @@ contract PatientManagement {
                 UserType.admin
             )
         );
+        patientList.push(
+            User(
+                1,
+                18,
+                2,
+                VaccineStatus.one_dose,
+                "Chittagong",
+                "Cough, Fever, Headache",
+                false,
+                address(0xD8e9698dbA5dF92882478e5f049b2666b00120d0),
+                UserType.patient
+            )
+        );
+        patientList.push(
+            User(
+                2,
+                82,
+                2,
+                VaccineStatus.one_dose,
+                "Chittagong",
+                "Fever, Cough",
+                false,
+                address(0x67A3688404800d48554448853e6e7028EDB8dEF9),
+                UserType.patient
+            )
+        );
+
+        patientList.push(
+            User(
+                3,
+                12,
+                1,
+                VaccineStatus.two_dose,
+                "Rajshahi",
+                "Fatigue, Loss of Taste",
+                true,
+                address(0x232Dccb4f2a2e57d085832aa8151Acd4dA5EC0cc),
+                UserType.patient
+            )
+        );
+        patientList.push(
+            User(
+                4,
+                28,
+                2,
+                VaccineStatus.not_vaccinated,
+                "Khulna",
+                "Body Aches, Sore Throat",
+                false,
+                address(0x54a231904E3D11447676BF6025aF240354011228),
+                UserType.patient
+            )
+        );
+
+        patientList.push(
+            User(
+                5,
+                45,
+                1,
+                VaccineStatus.one_dose,
+                "Dhaka",
+                "Shortness of Breath, Diarrhea",
+                false,
+                address(0xA43CaeD907c2987E655E5Fff06800021DCcC356b),
+                UserType.patient
+            )
+        );
+
         isAdmin[ownerAddress] = 1;
+        initialPatientId = 5;
     }
+    
 
     // Storing patient data
     function storePatientData(
@@ -95,7 +167,7 @@ contract PatientManagement {
 
         patientList.push(tempPatient);
         isAdmin[msg.sender] = 0;
-        isAlreadyPatient[msg.sender] = true;
+        isAlreadyPatient[msg.sender] = 1;
         initialPatientId++;
     }
 
@@ -126,14 +198,16 @@ contract PatientManagement {
         uint _patientId,
         VaccineStatus _vaccineStatus
     ) public onlyAdminAccess {
-        patientList[_patientId - 1].vaccineStatus = _vaccineStatus;
+        patientList[_patientId].vaccineStatus = _vaccineStatus;
+        emit updatedVaccineStatus(_patientId, _vaccineStatus);
     }
 
     function updateDeadStatus(
         uint _patientId,
         bool _deadStatus
     ) public onlyAdminAccess {
-        patientList[_patientId - 1].isDead = _deadStatus;
+        patientList[_patientId].isDead = _deadStatus;
+        emit updatedDeadStatus(_patientId, _deadStatus);
     }
 
     function getPatientDataList() public view returns (User[] memory) {
@@ -142,6 +216,13 @@ contract PatientManagement {
 
     function checkIfAdmin(address _adminAddress) public view returns (bool) {
         if (isAdmin[_adminAddress] == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    function checkIfAlreadyPatient(address _patientAddress) public view returns (bool) {
+        if (isAlreadyPatient[_patientAddress] == 1) {
             return true;
         }
         return false;
